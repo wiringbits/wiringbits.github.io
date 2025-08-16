55a629fbe8451da1e7864fe18651f64057a21a79
Alexis Hernandez <alexis22229@gmail.com>
2025-08-16 18:36:51 +0200

    Minor updates
    - Change page name/author.
    - Remove the talks section.
    - Display random posts in the home page.
    - Enable displaying the blog section in the header.
    - Set the correct domain.
    - Fix social links.

--- new-website/src/consts.ts
+++ new-website/src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alexis Hernandez';
-export const SITE_DESCRIPTION = 'My personal website';
-export const SITE_AUTHOR = 'Alexis Hernandez';
+export const SITE_TITLE = 'Wiringbits';
+export const SITE_DESCRIPTION = 'Wiringbits website';
+export const SITE_AUTHOR = 'Wiringbits';
