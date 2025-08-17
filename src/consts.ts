75e386d176ac9fb114710dc6d2d5b1d0affb880d
Alexis Hernandez <alexis22229@gmail.com>
2025-08-17 13:21:21 +0200

    Add footer section with company details

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,9 @@
 export const SITE_TITLE = 'Wiringbits';
 export const SITE_DESCRIPTION = 'Wiringbits website';
 export const SITE_AUTHOR = 'Wiringbits';
+
+export const COMPANY_NAME = 'Wiringbits';
+export const COMPANY_EMAIL = 'hello@wiringbits.net';
+export const COMPANY_ADDRESS_LINE1 = '180 N Michigan Ave Ste 625';
+export const COMPANY_ADDRESS_LINE2 = 'Chicago, IL, ZIP. 60601';
+export const COMPANY_ADDRESS_COUNTRY = 'United States';
